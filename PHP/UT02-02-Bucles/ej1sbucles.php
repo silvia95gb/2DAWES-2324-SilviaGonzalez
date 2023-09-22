1292b2260882986012e0e6d50160f8df947ad7a4
silvia95gb <125459353+silvia95gb@users.noreply.github.com>
2023-09-22 09:55:25 +0200

    Update ej1sbucles.php

--- PHP/UT02-02-Bucles/ej1sbucles.php
+++ PHP/UT02-02-Bucles/ej1sbucles.php
@@ -1,19 +1,24 @@
 <HTML>
-<HEAD><TITLE>EJ1B - Conversor decimal a binario</TITLE></HEAD>
+<HEAD><TITLE> EJ1B – Conversor decimal a binario</TITLE></HEAD>
 <BODY>
 
 <?php
 
-$num="168";
+//Programa ej1b.php: Transformar un número decimal a binario usando bucles 
+//(no se podrá utiliza la función decbin)
+
+//Creamos variable auxiliar para ir guardando el resto de num%2
+$num="19766";
 $aux="";
-echo "El numero en decimal es ".$num;
+
+echo "El numero en decimal es " .$num;
 echo "<br>";
 
-/* La variable num se actualiza con el cociente de cada division*/
+
 
 while ($num>=1) {
-	$aux=($num%2).$aux;
-	$num=$num/2;
+	$aux=($num%2).$aux; //con % obtienes el resto y concatenas el resultado
+	$num=$num/2; //mientras que se cumpla la condicion seguir haciendo la operacion
 }
 
 echo "El numero en binario es ".$aux;
