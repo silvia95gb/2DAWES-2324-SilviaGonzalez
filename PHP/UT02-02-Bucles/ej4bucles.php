8bdf1c47803d7a3663177db99554cde6f3c86952
silvia95gb <125459353+silvia95gb@users.noreply.github.com>
2023-09-22 19:42:01 +0200

    Create ej4bucles.php

---
+++ PHP/UT02-02-Bucles/ej4bucles.php
@@ -0,0 +1,17 @@
+<HTML>
+<HEAD><TITLE> EJ4B – Tabla Multiplicar</TITLE></HEAD>
+<BODY>
+<?php
+
+$num=8;
+echo "Está es la tabla de multiplicar del numero ".$num;
+echo"<br><br>";
+
+
+for ($i = 1; $i <= 10; $i++) {
+  echo $num. " x " .$i. " = " .$num*$i."<br>";
+}
+
+?>
+</BODY>
+</HTML>
