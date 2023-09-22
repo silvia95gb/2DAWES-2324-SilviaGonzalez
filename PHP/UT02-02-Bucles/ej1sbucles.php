c9716e5929379e71cb84a34fee2a5686576eaba8
silvia95gb <125459353+silvia95gb@users.noreply.github.com>
2023-09-22 09:21:37 +0200

    Update ej1sbucles.php

--- PHP/UT02-02-Bucles/ej1sbucles.php
+++ PHP/UT02-02-Bucles/ej1sbucles.php
@@ -1,1 +1,29 @@
+<HTML>
+<HEAD><TITLE>EJ1B - Conversor decimal a binario</TITLE></HEAD>
+<BODY>
 
+<?php
+
+$num="168";
+$aux="";
+echo "El numero en decimal es ".$num;
+echo "<br>";
+
+/* Esta parte del codigo es por si el numero en decimal es 0*/
+if($num==0){
+	$aux=0;
+}
+
+/* La variable num se actualiza con el cociente de cada division*/
+
+while ($num>=1) {
+	$aux=($num%2).$aux;
+	$num=$num/2;
+}
+
+echo "El numero en binario es ".$aux;
+
+?>
+
+</BODY>
+</HTML>
