2f675597783936dcfc66377c9281b814556d6f32
silvia95gb <125459353+silvia95gb@users.noreply.github.com>
2023-09-22 09:22:23 +0200

    Update ej1sbucles.php

--- PHP/UT02-02-Bucles/ej1sbucles.php
+++ PHP/UT02-02-Bucles/ej1sbucles.php
@@ -9,11 +9,6 @@ $aux="";
 echo "El numero en decimal es ".$num;
 echo "<br>";
 
-/* Esta parte del codigo es por si el numero en decimal es 0*/
-if($num==0){
-	$aux=0;
-}
-
 /* La variable num se actualiza con el cociente de cada division*/
 
 while ($num>=1) {
