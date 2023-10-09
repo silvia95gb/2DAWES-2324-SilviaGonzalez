e314788056cc8ba33d1b874ed32a08c002a22906
silvia95gb <125459353+silvia95gb@users.noreply.github.com>
2023-10-09 10:30:52 +0200

    Update calculadora01.php

--- PHP/UT02-04-Formularios/calculadora01.php
+++ PHP/UT02-04-Formularios/calculadora01.php
@@ -1,36 +1,41 @@
 <HTML>
-    <HEAD><TITLE> EJ1 - FORMULARIOS</TITLE>
+
+<HEAD>
+    <TITLE> EJ1 - FORMULARIOS</TITLE>
+   <?php include ('funciones/funcCalculadora.php'); //llama a las funciones
+   ?>
 </HEAD>
-    <BODY>
-        <h1>CALCULADORA</h1>
-        <form name="calculadora"  method="post" action="<?php echo $_SERVER['PHP_SELF'];?>"> //se llama a sí mismo
-            Operando1: <input type="text" name="operando1" value="" size="15"/>
 
-            <br><br>
-            Operando2: <input type="text" name="operando2" value="" size="15"/>
+<BODY>
+    <h1>CALCULADORA</h1>
+    <form name="calculadora" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Operando1: <input type="text" name="operando1" value="" size="15" />
 
-            <br><br>
-            Selecciona una operacion:<br>
-            <input type='radio' checked name='operacion' value='+'>Suma</br>
-            <input type='radio'  name='operacion' value='-'>Resta</br>
-            <input type='radio' name='operacion' value='*'>Producto</br>
-            <input type='radio' name='operacion' value='/'>Division</br>
+        <br><br>
+        Operando2: <input type="text" name="operando2" value="" size="15" />
 
-            <br>
-            <input type="submit" value="Enviar">
+        <br><br>
+        Selecciona una operacion:<br>
+        <input type='radio' checked name='operacion' value='+'>Suma</br>
+        <input type='radio' name='operacion' value='-'>Resta</br>
+        <input type='radio' name='operacion' value='*'>Producto</br>
+        <input type='radio' name='operacion' value='/'>Division</br>
 
-            <input type="reset" value="Borrar">
+        <br>
+        <input type="submit" value="Enviar">
 
-        </form>
+        <input type="reset" value="Borrar">
+
+    </form>
 
     <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $operando1 = test_input($_POST['operando1']);
+    $operando2 = test_input($_POST['operando2']);
+    $operacion = test_input($_POST['operacion']);
+    $resultado = 0;
+}
 
-$operando1 = $_POST['operando1'];
-$operando2 = $_POST['operando2'];
-$operacion = $_POST['operacion'];
-$resultado = 0;
-
-include ('funciones/funcCalculadora.php'); //llama a las funciones
 
 if ($operacion == "+"){
     $resultado = suma($operando1,$operando2);
@@ -46,6 +51,6 @@ echo "El resultado de la operación es: ".$operando1." ".$operacion." ".$operand
    
    
     ?>
-    </BODY>
-    </HTML>
-    
+</BODY>
+
+</HTML>
